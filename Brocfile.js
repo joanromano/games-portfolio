7373c5628535b08a46e106e4ca7d7d1523adedbc
Hector Zarco <hezarco@gmail.com>
2015-01-30 19:15:27 +0100

    Using Bourbon

--- Brocfile.js
+++ Brocfile.js
@@ -5,5 +5,6 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 var app = new EmberApp();
 
 app.import('bower_components/ember-backdoor/backdoor.js');
+app.import('bower_components/normalize.css/normalize.css');
 
 module.exports = app.toTree();
