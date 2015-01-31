c7c3327be20d1a35d66fb5abebb66d241864a435
Hector Zarco <hezarco@gmail.com>
2015-01-31 20:19:41 +0100

    Using rems instead of px

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,5 +3,18 @@ import Ember from "ember";
 export default Ember.Route.extend({
   beforeModel: function() {
     return this.get('store').find('game');
+  },
+
+  actions: {
+    historyBack: function() {
+      var history = window.history;
+      //In case of history api it's not supported (IE)
+      if (!history) {
+        this.transitionTo('all');
+        return;
+      }
+
+      history.back();
+    }
   }
 });
