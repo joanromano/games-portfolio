1ebd1b98021e66a83b7fe53594175f7802a41858
Hector Zarco <hezarco@gmail.com>
2015-01-31 19:16:53 +0100

    Export game-card as component

--- app/components/game-card.js
+++ app/components/game-card.js
@@ -1,5 +1,13 @@
 import Ember from "ember";
 
 export default Ember.Component.extend({
+  classNames: ['game-card-component'],
+  classNameBindings: ['content.isVisible'],
+  content: null,
 
+  actions: {
+    toggleFavorite: function() {
+      this.toggleProperty('content.isFavorite');
+    }
+  }
 });
